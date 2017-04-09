5dac62ad6c4262192b5684f0cedfa77901bae40c
mtso <mtso@tuta.io>
2017-04-08 17:30:55 -0700

    Configure webpack for jsx

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const indexInjector = new HtmlWebpackPlugin({
+  filename: 'index.html',
+  template: './app/index.template',
+  inject: 'body',
+});
+
+module.exports = {
+  entry: './app/index',
+  resolve: {
+    extensions: ['.jsx', '.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: ['babel-loader?presets[]=react,presets[]=es2015'],
+      },
+    ],
+  },
+  plugins: [
+    indexInjector,
+  ],
+}
