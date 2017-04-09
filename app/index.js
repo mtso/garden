3860291e3d1cc67f72dacade9ca2454dc8ba288c
mtso <mtso@tuta.io>
2017-04-08 18:30:13 -0700

    Handle keydown events

--- app/index.js
+++ app/index.js
@@ -1,15 +1,16 @@
-window.addEventListener('keydown', (event) => {
-  console.log(event.keyCode)
-})
-
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Scene from './components/Scene';
 
+const DOWN = 40
+const LEFT = 37
+const RIGHT = 39
+const UP = 38
+
 let grid = [
   ['.', '.', '.', '.', '.', '.'],
   ['.', '.', '.', '.', '.', '.'],
-  ['.', '.', '.', 'S', '.', '.'],
+  ['.', '.', '.', '.', '.', '.'],
   ['.', '.', '.', '.', '.', '.'],
   ['.', '.', '.', '.', '.', '.'],
   ['.', '.', '.', '.', '.', '.'],
@@ -21,19 +22,48 @@ class Grid extends Component {
       return row.join(' ')
     }).join('\n')
     return (
-      <pre>{grid}</pre>
+      <pre style={{fontFamily: '"Monaco", "Consolas", monospace'}}>{grid}</pre>
     )
   }
 }
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      grid: grid,
+      playerPos: {x: 0, y: 0}
+    }
+    this.move = this.move.bind(this)
+  }
+  move(event) {
+    let direction = event.keyCode
+    switch (direction) {
+      case DOWN:
+        var nextPos = {
+          x: this.state.playerPos.x,
+          y: this.state.playerPos.y + 1
+        }
+        let newGrid = this.state.grid;
+        newGrid[nextPos.y][nextPos.x] = 'S'
+        this.setState({
+          playerPos: nextPos,
+          grid: newGrid,
+        })
+    }
+  }
   render() {
     return (
-      <div>
-        <Grid grid={grid} />
+      <div tabIndex='0' onKeyDown={this.move}>
+        <Grid grid={this.state.grid} />
       </div>
     )
   }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
+
+// window.addEventListener('keydown', (event) => {
+//   console.log(event.keyCode)
+//   app.move(event.keyCode)
+// })
